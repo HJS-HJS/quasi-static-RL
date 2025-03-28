5696cdbdb7ba750dc237b8728df9197ae8af5484
HJS <jongsoo@g.skku.edu>
2025-03-28 18:28:34 +0900

    [add] server

--- src/train_test_server3.py
+++ src/train_test_server3.py
@@ -14,7 +14,7 @@ import time
 so_file_path = os.path.abspath("../cpp")
 sys.path.append(so_file_path)
 
-from utils.simulation_cpp_pre_curriculum import DishSimulation
+from utils.simulation_server2 import DishSimulation
 
 from utils.sac_dataset_cpp_linear import SACDataset
 from utils.utils           import *
